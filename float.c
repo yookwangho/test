89e064a64473bfbe91c8ca2fb3556879adf4a95f
yookwangho <yoogwangho@naver.com>
2016-09-27 23:57:08 +0900

    Add files via upload

---
+++ float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x = 3.14f;
+	double y = -3.141592;
+	long double z = 180000000.0;
+	printf("ÀúÀå°ª:%f %f %f\n",x,y,z);
+	return 0;
+}
---
+++ var.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int snum;
+	int credits;
+
+	snum = 20163021;
+	credits = 18;
+
+	printf("학번:%\n", snum);
+	printf("신청학점:%d\n", credits);
+
+	
+	return 0;
+}
